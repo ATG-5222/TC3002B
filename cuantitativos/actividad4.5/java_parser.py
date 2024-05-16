ec3c716cdb500661428b28faf205950b51ee4a6e
ATG-5222 <A01275222@tec.mx>
2024-05-16 17:42:34 -0600

    Añadir nombre de archivo en java_parser

--- cuantitativos/actividad4.5/java_parser.py
+++ cuantitativos/actividad4.5/java_parser.py
@@ -1,10 +1,12 @@
 import javalang
 
+# Definir el nombre del archivo de java
+java_file = "example_1.java"
 # Cargar el archivo Java
-with open('HelloWorld.java', 'r') as file:
-    codigo_java = file.read()
+with open(java_file, 'r') as file:
+    java_code = file.read()
 # Analizar el código Java
-tokens = javalang.tokenizer.tokenize(codigo_java)
+tokens = javalang.tokenizer.tokenize(java_code)
 # Imprimir los tokens
 for token in tokens:
     print(f"Token: {token.value}, Tipo: {token.__class__.__name__}")

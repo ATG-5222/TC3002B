1e9124c8cc49d4e8a410295828163e4de31ad2dd
ATG-5222 <A01275222@tec.mx>
2024-05-19 22:01:10 -0600

    Modificar ruta de main.py

--- compilers/stage-1/main.py
+++ compilers/stage-1/main.py
@@ -2,11 +2,9 @@ from Lexer import *
 import sys
 
 if __name__ == '__main__':
-	lexer = Lexer("test_cases/prog1.txt")
-	
+	lexer = Lexer("test_cases/good/prog1.txt")
 	token = lexer.scan()
 	while token.getTag() != Tag.EOF:
 		print(str(token))
 		token = lexer.scan()
 	print("END")
-

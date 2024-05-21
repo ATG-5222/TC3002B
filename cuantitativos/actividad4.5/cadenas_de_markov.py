ce972aabb41d84e48ea3347d28ed0364af1a2315
ATG-5222 <A01275222@tec.mx>
2024-05-21 13:28:26 -0600

    Generar grafos basados en los tokens

--- cuantitativos/actividad4.5/cadenas_de_markov.py
+++ cuantitativos/actividad4.5/cadenas_de_markov.py
@@ -1,26 +1,46 @@
-# Generar diagrama de transiciones
-def transition_diagram(text):
-    transitions = {}
-    for i in range(len(text) - 1):
-        current_transition = (text[i], text[i + 1])
-        if current_transition in transitions:
-            transitions[current_transition] += 1
-        else:
-            transitions[current_transition] = 1
-    return transitions
+import re
+import numpy as np
+import math
+import javalang
 
-# Comparar las similitudes entre los diagramas de transición
-def similarity_transition_diagram(text1, text2):
-    diagram1 = transition_diagram(text1)
-    diagram2 = transition_diagram(text2)
-    intersection = set(diagram1.keys()) & set(diagram2.keys())
-    total_transitions1 = sum(diagram1.values())
-    total_transitions2 = sum(diagram2.values())
-    similarity = sum(min(diagram1.get(t, 0), diagram2.get(t, 0)) for t in intersection) / max(total_transitions1, total_transitions2)
-    return similarity
+# Leer y almacenar el texto 1 
+with open("example_1.java", 'r', encoding='utf-8') as f:
+    text1 = f.read()
 
-# Ejemplo de uso
-text1 = "hola"
-text2 = "hola"
-similarity = similarity_transition_diagram(text1, text2)
-print("Similitud entre los textos:", similarity)
+# Leer y almacenar el texto 2 
+with open("example_2.java", 'r', encoding='utf-8') as f:
+    text2 = f.read()
+
+# Función para tokenizar utilizando javalang
+def tokenize_with_javalang(text):
+    tokens = []
+    lexer = javalang.tokenizer.tokenize(text)
+    for token in lexer:
+        tokens.append(token.value)
+    return tokens
+
+# Tokenizar ambos textos
+tokens_text1 = tokenize_with_javalang(text1)
+tokens_text2 = tokenize_with_javalang(text2)
+
+# Combinar los tokens de ambos textos
+combined_text = tokens_text1 + tokens_text2
+
+words1 = {}
+words2 = {}
+
+for word in combined_text:
+    words1[word] = []
+    words2[word] = []
+
+# Crear grafo para text1
+for i in range(1, len(tokens_text1)):
+    origin_word = tokens_text1[i-1]
+    destination_word = tokens_text1[i]
+    words1[origin_word].append(destination_word)
+
+# Crear grafo para text2
+for i in range(1, len(tokens_text2)):
+    origin_word = tokens_text2[i-1]
+    destination_word = tokens_text2[i]
+    words2[origin_word].append(destination_word)

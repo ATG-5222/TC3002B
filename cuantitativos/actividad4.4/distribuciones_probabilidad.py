a3494be043e85cb002ebcdbe7e57d1ad8e1b6db8
ATG-5222 <A01275222@tec.mx>
2024-05-30 16:48:42 -0600

    Añadir textos adicionales

--- cuantitativos/actividad4.4/distribuciones_probabilidad.py
+++ cuantitativos/actividad4.4/distribuciones_probabilidad.py
@@ -8,14 +8,25 @@ def read_text_file(file_path):
     with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
         return file.read()
 
-# Ruta al archivo de texto corpus
-corpus_file_path = "corpus.txt"
+# Función para calcular la similitud del coseno
+def cosineSimilarity(text1, text2):
+    # Almacenar ambos textos juntos
+    texts = [text1, text2]
+    # Transformar ambos textos a una representación vectorial usando tf-idf
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(texts)
+    # Calcular la similitud usando la distancia del coseno
+    similarity_matrix = cosine_similarity(tfidf_matrix)
+    return similarity_matrix
+
+# Ruta al archivo del corpus de texto zaratustras
+corpus_file_path = "zaratustra.txt"
 
 # Leer el texto original
-original_text = read_text_file(corpus_file_path)
+zaratustra = read_text_file(corpus_file_path)
 
 # Generar y almacenar el texto sintético usando markovify
-model = markovify.Text(original_text)
+model = markovify.Text(zaratustra)
 synthetic_text = ''
 for i in range(20):
     sentence = model.make_sentence()
@@ -24,24 +35,51 @@ for i in range(20):
     synthetic_text += str(sentence)
 
 # Imprimir ambos textos
+print("\n")
 print("---------------")
 print("Texto original:")
-print(original_text)
+print(zaratustra)
 print("---------------")
 print("Texto generado:")
 print(synthetic_text)
 print("---------------")
 
-# Almacenar ambos textos juntos
-texts = [original_text, synthetic_text]
-
-# Transformar ambos textos a una representación vectorial usando tf-idf
-vectorizer = TfidfVectorizer()
-tfidf_matrix = vectorizer.fit_transform(texts)
-
-# Calcular la similitud usando la distancia del coseno
-similarity_matrix = cosine_similarity(tfidf_matrix)
-
 # Similitud entre el texto original y el texto sintético
-similarity_synthetic_original = similarity_matrix[0][1]
-print(f"Similitud entre el texto original y el texto sintético: {similarity_synthetic_original}")
+similarity_matrix_1 = cosineSimilarity(zaratustra, synthetic_text)
+similarity_synthetic_original = similarity_matrix_1[0][1]
+print(f"Similitud entre el texto original (Extracto de : 'Así hablaba zaratustra') y el texto sintético: {similarity_synthetic_original}")
+print("\n")
+
+# Comparar textos identicos (fragmento de "La llamada de Cthulhu")
+
+# Ruta al archivo del corpus de texto llamada_de_cthulhu
+corpus_file_path_2 = "llamada_de_cthulhu.txt"
+
+# Leer el texto original
+cthulhu = read_text_file(corpus_file_path_2)
+
+# Similitud entre ambos textos
+similarity_matrix_2 = cosineSimilarity(cthulhu, cthulhu)
+similarity_equals = similarity_matrix_2[0][1]
+print(f"Similitud entre el texto (Extracto de : 'La llamada de Cthulhu') y el mismo: {similarity_equals}")
+print("\n")
+
+# Comparar textos diferentes "La llamada de Cthulhu" y "La llave de plata"
+
+# Ruta al archivo del corpus de texto llamada_de_cthulhu
+corpus_file_path_3 = "llamada_de_cthulhu.txt"
+
+# Ruta al archivo del corpus de texto llave_de_plata
+corpus_file_path_4 = "llave_de_plata.txt"
+
+# Leer texto 1 
+cthulhu = read_text_file(corpus_file_path_3)
+
+# Leer texto 2
+llave = read_text_file(corpus_file_path_4)
+
+# Similitud entre ambos textos
+similarity_matrix_3 = cosineSimilarity(cthulhu, llave)
+similarity_differents = similarity_matrix_3[0][1]
+print(f"Similitud entre el texto 1 (Extracto de : 'La llamada de Cthulhu') y el texto 2 (Extracto de : 'La lave de plata'): {similarity_differents}")
+print("\n")
